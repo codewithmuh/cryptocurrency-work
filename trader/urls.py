7b6357f729608eb5ca77bb6681d1eef422976f21
amisdun <michealdunamis@gmail.com>
2020-10-29 16:21:03 +0000

    changes made to update settings

--- trader/urls.py
+++ trader/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from trader.views.settings import settings_view, user_profile_settings
+from trader.views.settings import settings_view, user_profile_settings, user_update_description
 from trader.views.dashboard import dashboard
 from trader.views.marketplace import marketplace
 from trader.views.crypto_trading import copy_trading
@@ -12,5 +12,7 @@ urlpatterns = [
     path('marketplace/',marketplace,name="marketplace"),
     path('copyTrading/',copy_trading,name="copyTrading"),
     path('profile/',profile,name="profile"),
-    path('settings/', settings_view, name="settings")
+    path('settings/', settings_view, name="settings"),
+    path('settings/update_description/', user_update_description, name="user_update_description"),
+    path('settings/user_update/', user_profile_settings , name='user_update')
 ]

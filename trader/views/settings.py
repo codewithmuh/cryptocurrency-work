4f74927ac4d3a20aee1e5b2b325ccb0a252382e4
amisdun <michealdunamis@gmail.com>
2020-10-29 16:19:09 +0000

    update on user profile and user description

--- trader/views/settings.py
+++ trader/views/settings.py
@@ -2,11 +2,53 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from accounts.models.user_profile import UserProfile
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+@login_required(login_url="login_view/")
+def user_update_description(request):
+    if request.method == "POST":
+        context = {}
+        template_name = "settings.html"
+        body = request.POST
+
+        description = body.get('description', '')
+
+        user = User.objects.get(pk=request.user.id)
+        user_profile = UserProfile.objects.filter(user=user).first()
+
+        if user:
+            user_profile.description = description
+            user_profile.save()
+            context['user_profile'] = user_profile
+            messages.success(request, template_name=template_name, context=context)
+        return render(request, template_name=template_name, context=context)
+
 
 @login_required(login_url="login_view/")
 def user_profile_settings(request):
+    if request.method == "POST":
+        context = {}
+        template_name = "settings.html"
+        body = request.POST
 
-    pass
+        user = User.objects.get(pk=request.user.id)
+        user_profile = UserProfile.objects.filter(user=user).first()
+
+        first_name = body.get('first_name', '')
+        last_name = body.get('last_name', '')
+
+        if user:
+            user_profile.user.first_name = first_name
+            user_profile.user.last_name = last_name
+
+            user_profile.user.save()
+            user_profile.save()
+
+            context['user_profile'] = user_profile
+
+            messages.success(request, "Updated successfully")
+
+        return render(request, template_name=template_name, context=context)
 
 
 def settings_view(request):

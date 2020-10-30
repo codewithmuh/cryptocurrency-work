93ae7a75fcaf4282753b542473152f83bcae553a
amisdun <michealdunamis@gmail.com>
2020-10-30 06:31:10 +0000

    changes applied

--- accounts/models/traders/traders_account.py
+++ accounts/models/traders/traders_account.py
@@ -1,7 +1,7 @@
 from accounts.models.user_profile import UserProfile
 from django.db import models
 from .exchange import Exchange
-from accounts.models.traders.base_currency import BaseCurrency
+from .base_currency import BaseCurrency
 
 
 class TraderAccounts(models.Model):
@@ -13,4 +13,28 @@ class TraderAccounts(models.Model):
     base_currency = models.ForeignKey(BaseCurrency, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
+    @classmethod
+    def get_or_create_trader_account(cls, trader, account_name, api_key, api_secret, exchange, base_currency
+                                     ):
+        get_exchange = Exchange.objects.filter(pk=exchange).first()
+        get_currency = BaseCurrency.objects.filter(pk=base_currency).first()
+
+        trader_account = cls.objects.filter(account_name=account_name).first()
+
+        if trader_account is not None:
+            response = "exist"
+        else:
+            cls.objects.create(
+                trader=trader,
+                account_name=account_name,
+                api_key=api_key,
+                api_secret=api_secret,
+                exchange=get_exchange,
+                base_currency=get_currency
+            )
+
+            response = "saved"
+            
+        return response
+

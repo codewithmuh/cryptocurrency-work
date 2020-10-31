9ab8c196524cad003e517fb8c22337c64bb5796b
amisdun <michealdunamis@gmail.com>
2020-10-31 10:33:47 +0000

    changes made to traders url

--- trader/urls.py
+++ trader/urls.py
@@ -4,7 +4,7 @@ from trader.views.dashboard import dashboard
 from trader.views.marketplace import marketplace
 from trader.views.crypto_trading import copy_trading
 from trader.views.profile import profile
-from .views.traders_account import create_trader_account
+from .views.traders_account import create_trader_account, delete_trader_account, update_trader_account
 
 
 
@@ -16,5 +16,7 @@ urlpatterns = [
     path('settings/', settings_view, name="settings"),
     path('settings/update_description/', user_update_description, name="user_update_description"),
     path('settings/user_update/', user_profile_settings , name='user_update'),
-    path('settings/create_trader_account/', create_trader_account, name="create_trader_Account")
+    path('settings/create_trader_account/', create_trader_account, name="create_trader_Account"),
+    path('settings/update_trader_account/<int:trader_id>/', update_trader_account, name="update_trader_Account"),
+    path('settings/delete_trader_account/<int:trader_id/', delete_trader_account, name="delete_trader_Account")
 ]
